73e08fb2e216e459ae442f53574ba435c894310f
Rochdi GUEZGUEZ <Rochdi.GUEZGUEZ@Gmail.Com>
2022-11-26 10:50:25 +0100

    Add the project files

--- frontend/js/fetch_data.js
+++ frontend/js/fetch_data.js
@@ -128,37 +128,3 @@ function infoFilm (element, url){
             .catch(function(err){})
     })
 }
-
-// Fonction pour éviter la propagation du clic pour les flèches
-let cursorsLeft = document.getElementsByClassName("cursor-left");
-for (cursorLeft of cursorsLeft){
-    cursorLeft.addEventListener("click", function(event){
-        event.stopPropagation();
-    })
-}
-let cursorsRight = document.getElementsByClassName("cursor-right");
-for (cursorRight of cursorsRight){
-    cursorRight.addEventListener("click", function(event){
-        event.stopPropagation();
-    })
-}
-
-// Fonction pour le défilement latéral(fleches)
-let bestFilms = document.getElementsByClassName("sevenBest")[0];
-let zoneComedy = document.getElementsByClassName("comedy")[0];
-let zoneFamily = document.getElementsByClassName("family")[0];
-let zoneAction = document.getElementsByClassName("action")[0];
-function goLeft(zone){
-    zone.scrollBy({
-        left : -300,
-        behavior : "smooth"
-    }
-    )
-}
-function goRight(zone){
-    zone.scrollBy({
-        left : 300,
-        behavior : "smooth"
-    }
-    )
-}
